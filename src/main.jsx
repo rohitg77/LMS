30d764a67b0e199c0732da2acf7b839a1853f446
Pratham <122166099+pratham40@users.noreply.github.com>
2024-08-30 23:56:13 +0530

    add reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
-import './index.css'
+import "./index.css";
 
-import { createRoot } from 'react-dom/client'
-import { Toaster } from 'react-hot-toast'
-import { BrowserRouter } from "react-router-dom"
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import store from "./Redux/Store.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <BrowserRouter>
-    <App />
-    <Toaster/>
+      <App />
+      <Toaster />
     </BrowserRouter>
-)
+  </Provider>
+);
