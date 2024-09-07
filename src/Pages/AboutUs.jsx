ba07d13f731f5b40f57a85b5fa166d2cc55b4b88
Pratham <122166099+pratham40@users.noreply.github.com>
2024-09-07 23:09:48 +0530

    clean up about us page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,11 +1,6 @@
-import React from "react";
-
 import aboutMainImage from "../Assets/Images/aboutMainImage.png";
-import apj from "../Assets/Images/QuotesPersonalityImage/apj.png";
-import billGates from "../Assets/Images/QuotesPersonalityImage/billGates.png";
-import einstein from "../Assets/Images/QuotesPersonalityImage/einstein.png";
-import nelsonMandela from "../Assets/Images/QuotesPersonalityImage/nelsonMandela.png";
-import steveJobs from "../Assets/Images/QuotesPersonalityImage/steveJobs.png";
+import CarouselSlide from "../Components/CarouselSlide";
+import {celeb} from "../constants/CelebrityData"
 import HomeLayout from "../Layout/HomeLayout";
 function AboutUs() {
   return (
@@ -25,117 +20,25 @@ function AboutUs() {
           </section>
           <div className="w-1/2">
             <img
-                id="test1"
-                style={{
-                    filter: "drop-shadow(0px,10px,10px,rgb(0,0,0))",
-                }}
+              id="test1"
+              style={{
+                filter: "drop-shadow(0px,10px,10px,rgb(0,0,0))",
+              }}
               className="drop-shadow-2xl"
               src={aboutMainImage}
               alt="aboutMainImage"
             />
           </div>
         </div>
-        <div className="carousel w-1/2 m-auto">
-          <div id="slide1" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={apj}
-                className="w-40 rounded-full border-2 border-gray-400"
+        <div className="carousel w-1/2 m-auto my-16">
+          {celeb &&
+            celeb.map(cele => (
+              <CarouselSlide
+                {...cele}
+                key={cele.slideNumber}
+                totalSlide={celeb.length}
               />
-              <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-              <p className="text-xl text-gray-200">
-                {'"Education is the most powerful tool you can use to change the world."'}
-              </p>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide5" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide2" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide2" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={einstein}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <h3 className="text-2xl font-semibold">Albert Einstein</h3>
-              <p className="text-xl text-gray-200">
-                {'"Education is not the learning of facts, but the training of the mind to think."'}
-              </p>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide1" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide3" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide3" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={steveJobs}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-              <p className="text-xl text-gray-200">
-                {'"Innovation distinguishes between a leader and a follower."'}
-              </p>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide2" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide4" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide4" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={billGates}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-              <h3 className="text-2xl font-semibold">Bill Gates</h3>
-              <p className="text-xl text-gray-200">
-                {'"Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important."'}
-              </p>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide3" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide5" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
-          <div id="slide5" className="carousel-item relative w-full">
-            <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-              <img
-                src={nelsonMandela}
-                className="w-40 rounded-full border-2 border-gray-400"
-              />
-               <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-              <p className="text-xl text-gray-200">
-                {'"Education is the most powerful tool you can use to change the world."'}
-              </p>
-              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                <a href="#slide4" className="btn btn-circle">
-                  ❮
-                </a>
-                <a href="#slide1" className="btn btn-circle">
-                  ❯
-                </a>
-              </div>
-            </div>
-          </div>
+            ))}
         </div>
       </div>
     </HomeLayout>
